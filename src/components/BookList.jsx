c34de6354de904d4e6f0645ba10ab57a871ec3aa
sanandreas11 <andreaandonis@outlook.com>
2025-04-02 18:00:24 +0200

    updates exercise

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,16 +1,46 @@
-import { Container, Row } from "react-bootstrap"
+import { Col, Container, Form, Row } from "react-bootstrap"
 import SingleBook from "./SingleBook"
+import { Component } from "react"
 
-const BookList = function (props) {
-  return (
-    <Container>
-      <Row>
-        {props.arrayOfBooks.map((book) => {
-          return <SingleBook book={book} key={book.asin} />
-        })}
-      </Row>
-    </Container>
-  )
+class BookList extends Component {
+  state = {
+    search: "",
+  }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xs={12} md={6}>
+            <Form.Control
+              type="text"
+              placeholder="cerca un libro"
+              value={this.state.search}
+              onChange={(e) => {
+                this.setState({ search: e.target.value })
+              }}
+            />
+          </Col>
+        </Row>
+        <Row>
+          {this.props.arrayOfBooks
+            .filter((book) => {
+              if (
+                book.title
+                  .toLowerCase()
+                  .includes(this.state.search.toLowerCase())
+              ) {
+                return true
+              } else {
+                return false
+              }
+            })
+            .map((book) => {
+              return <SingleBook book={book} key={book.asin} />
+            })}
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default BookList
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,20 +1,35 @@
+import { Component } from "react"
 import { Card, Button, Col } from "react-bootstrap"
 
-const SingleBook = function (props) {
-  return (
-    <Col xs={12} s={6} md={4} lg={3}>
-      <Card>
-        <Card.Img variant="top" src={props.book.img} />
-        <Card.Body>
-          <Card.Title>{props.book.title}</Card.Title>
-          <Card.Text>
-            {props.book.category} - {props.book.price}
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  )
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  }
+
+  render() {
+    return (
+      <Col xs={12} s={6} md={4} lg={3}>
+        <Card className={this.state.selected ? "selectedBook" : ""}>
+          <Card.Img
+            variant="top"
+            src={this.props.book.img}
+            onClick={() => {
+              this.setState({
+                selected: !this.state.selected, //toggle
+              })
+            }}
+          />
+          <Card.Body>
+            <Card.Title>{this.props.book.title}</Card.Title>
+            <Card.Text>
+              {this.props.book.category} - {this.props.book.price}
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  }
 }
 
 export default SingleBook
